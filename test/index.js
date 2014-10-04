0c2e96d6630ae0f190c5db6fd628ef2f2390d676
ali1k <hyperir@gmail.com>
2014-10-04 11:15:57 +0200

    todo: adding some tests

---
+++ test/index.js
@@ -0,0 +1,12 @@
+//TODO: write some test cases
+var should = require('chai').should(),
+    mlspotlight = require('../index'),
+    annotate = mlspotlight.annotate;
+
+describe('#annotate', function() {
+  it('annotate a sample text in Engligh;', function() {
+    var input='test';
+    var output='';
+    annotate(input).should.equal(output);
+  });
+});
